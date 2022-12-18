46431d003c69fdcf61936486bfc46330b99c3288
Juan <6652831+quadlayers@users.noreply.github.com>
2022-12-18 18:48:25 -0300

    Align rest api to WC standard functionality

--- classes/class-pwb-api-support.php
+++ classes/class-pwb-api-support.php
@@ -2,18 +2,25 @@
 
 namespace Perfect_Woocommerce_Brands;
 
-use WP_Error, WP_REST_Server;
+use WP_Error, WP_REST_Server, WP_REST_Terms_Controller, WP_REST_Term_Meta_Fields;
 
-defined( 'ABSPATH' ) || die( 'No script kiddies please!' );
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-class PWB_API_Support {
+class PWB_API_Support extends WP_REST_Terms_Controller {
 
 	private $namespaces = array( 'wc/v1', 'wc/v2', 'wc/v3' );
-	private $base       = 'brands';
+	protected $base     = 'brands';
+	protected $taxonomy = 'pwb-brand';
 
 	function __construct() {
+		$this->meta = new WP_REST_Term_Meta_Fields( $this->taxonomy );
+
 		add_action( 'rest_api_init', array( $this, 'register_endpoints' ) );
 		add_action( 'rest_api_init', array( $this, 'register_fields' ) );
+
+		add_filter( "rest_{$this->taxonomy}_collection_params", array( $this, 'modify_collection_params' ), 10, 2 );
+		add_filter( 'woocommerce_rest_product_object_query', array( $this, 'brand_query_args' ), 10, 2 );
+
 	}
 
 	/**
@@ -23,66 +30,165 @@ class PWB_API_Support {
 		foreach ( $this->namespaces as $namespace ) {
 			register_rest_route(
 				$namespace,
-				'/' . $this->base,
+				$this->base,
 				array(
 					array(
 						'methods'             => WP_REST_Server::READABLE,
-						'callback'            => function () {
-							return rest_ensure_response(
-								Perfect_Woocommerce_Brands::get_brands()
-							);
-						},
-						'permission_callback' => '__return_true',
+						'callback'            => array( $this, 'get_items' ),
+						'permission_callback' => array( $this, 'get_items_permissions_check' ),
+						'args'                => $this->get_collection_params(),
 					),
 					array(
 						'methods'             => WP_REST_Server::CREATABLE,
-						'callback'            => array( $this, 'create_brand' ),
-						'permission_callback' => function () {
-							return current_user_can( 'manage_options' );
-						},
+						'callback'            => array( $this, 'create_item' ),
+						'permission_callback' => array( $this, 'create_item_permissions_check' ),
+						'args'                => array_merge(
+							$this->get_endpoint_args_for_item_schema( WP_REST_Server::CREATABLE ),
+							array(
+								'name' => array(
+									'description' => __( 'Brand name.', 'perfect-woocommerce-brands' ),
+									'required'    => true,
+									'type'        => 'string',
+								),
+							)
+						),
+					),
+					'schema' => array( $this, 'get_public_item_schema' ),
+				)
+			);
 
+			register_rest_route(
+				$namespace,
+				$this->base . '/(?P<id>[\d]+)',
+				array(
+					'args'   => array(
+						'id' => array(
+							'description' => __( 'Unique identifier for the resource.', 'perfect-woocommerce-brands' ),
+							'type'        => 'integer',
+						),
+					),
+					array(
+						'methods'             => WP_REST_Server::READABLE,
+						'callback'            => array( $this, 'get_item' ),
+						'permission_callback' => array( $this, 'get_item_permissions_check' ),
+						'args'                => array(
+							'context' => $this->get_context_param( array( 'default' => 'view' ) ),
+						),
+					),
+					array(
+						'methods'             => WP_REST_Server::EDITABLE,
+						'callback'            => array( $this, 'update_item' ),
+						'permission_callback' => array( $this, 'update_item_permissions_check' ),
+						'args'                => $this->get_endpoint_args_for_item_schema( WP_REST_Server::EDITABLE ),
 					),
 					array(
 						'methods'             => WP_REST_Server::DELETABLE,
-						'callback'            => array( $this, 'delete_brand' ),
-						'permission_callback' => function () {
-							return current_user_can( 'manage_options' );
-						},
+						'callback'            => array( $this, 'delete_item' ),
+						'permission_callback' => array( $this, 'delete_item_permissions_check' ),
+						'args'                => array(
+							'force' => array(
+								'default'     => true,
+								'type'        => 'boolean',
+								'description' => __( 'Whether to bypass trash and force deletion.', 'perfect-woocommerce-brands' ),
+							),
+						),
 					),
+					'schema' => array( $this, 'get_public_item_schema' ),
+				)
+			);
+
+			register_rest_route(
+				$namespace,
+				$this->base . '/batch',
+				array(
+					array(
+						'methods'             => WP_REST_Server::EDITABLE,
+						'callback'            => array( $this, 'batch_items' ),
+						'permission_callback' => array( $this, 'batch_items_permissions_check' ),
+						'args'                => $this->get_endpoint_args_for_item_schema( WP_REST_Server::EDITABLE ),
+					),
+					'schema' => array( $this, 'get_public_batch_schema' ),
 				)
 			);
 		}
 	}
 
-	public function delete_brand( $request ) {
-		foreach ( $request['brands'] as $brand ) {
-			$delete_result = wp_delete_term( $brand, 'pwb-brand' );
-			if ( is_wp_error( $delete_result ) ) {
-				return $delete_result;
-			}
-		}
-		return true;
+
+	public function modify_collection_params( $query_params, $taxonomy ) {
+		unset( $query_params['post'] );
+		unset( $query_params['per_page'] );
+
+		$query_params['product'] = array(
+			'description' => __( 'Limit result set to terms assigned to a specific product.' ),
+			'type'        => 'integer',
+			'default'     => null,
+		);
+
+		$query_params['per_page'] = array(
+			'description'       => __( 'Maximum number of items to be returned in result set.' ),
+			'type'              => 'integer',
+			'default'           => 1000,
+			'minimum'           => 1,
+			'maximum'           => 1000,
+			'sanitize_callback' => 'absint',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+
+		return $query_params;
 	}
 
-	public function create_brand( $request ) {
-		$new_brand = wp_insert_term(
-			$request['name'],
-			'pwb-brand',
-			array(
-				'slug'        => $request['slug'],
-				'description' => $request['description'],
-			)
+
+	/**
+	 * Prepare a single brand output for response.
+	 *
+	 * @param WP_Term         $item    Term object.
+	 * @param WP_REST_Request $request Request instance.
+	 * @return WP_REST_Response
+	 */
+	public function prepare_item_for_response( $item, $request ) {
+		$data = array(
+			'term_id'          => (int) $item->term_id,
+			'name'             => $item->name,
+			'slug'             => $item->slug,
+			'term_group'       => $item->term_group,
+			'term_taxonomy_id' => $item->term_taxonomy_id,
+			'taxonomy'         => $this->taxonomy,
+			'description'      => $item->description,
+			'parent'           => $item->parent,
+			'count'            => (int) $item->count,
+			'filter'           => $item->filter,
 		);
-		if ( ! is_wp_error( $new_brand ) ) {
-			return array(
-				'id'          => $new_brand['term_id'],
-				'name'        => $request['name'],
-				'slug'        => $request['slug'],
-				'description' => $request['description'],
-			);
-		} else {
-			return $new_brand;
-		}
+
+		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
+		$data    = $this->add_additional_fields_to_object( $data, $request );
+		$data    = $this->filter_response_by_context( $data, $context );
+
+		$response = rest_ensure_response( $data );
+
+		/**
+		 * Filter a term item returned from the API.
+		 *
+		 * Allows modification of the term data right before it is returned.
+		 *
+		 * @param WP_REST_Response  $response  The response object.
+		 * @param object            $item      The original term object.
+		 * @param WP_REST_Request   $request   Request used to generate the response.
+		 */
+		return apply_filters( "woocommerce_rest_prepare_{$this->taxonomy}", $response, $item, $request );
+	}
+
+	/**
+	 * Add image fields to term item returned form the API
+	 */
+	public function add_additional_fields_to_object( $item, $request ) {
+
+		$brand_image_id       = get_term_meta( $item['term_id'], 'pwb_brand_image', true );
+		$brand_banner_id      = get_term_meta( $item['term_id'], 'pwb_brand_banner', true );
+		$item['brand_image']  = wp_get_attachment_image_src( $brand_image_id );
+		$item['brand_banner'] = wp_get_attachment_image_src( $brand_banner_id );
+
+		return $item;
+
 	}
 
 	/**
@@ -108,8 +214,9 @@ class PWB_API_Support {
 	 * @return array
 	 */
 	public function get_schema() {
+
 		return array(
-			'description' => __( 'Product brands', 'perfect-woocommerce-brands' ),
+			'description' => __( 'Product brands.', 'perfect-woocommerce-brands' ),
 			'type'        => 'array',
 			'items'       => array(
 				'type' => 'integer',
@@ -171,5 +278,31 @@ class PWB_API_Support {
 	 */
 	private function add_brands( $brands, $product ) {
 		wp_set_post_terms( $product->get_id(), $brands, 'pwb-brand' );
+	}
+
+	/**
+	 * Add brands to product query args
+	 *
+	 * @param array $args
+	 * @param array $request
+	 */
+	public function brand_query_args( $args, $request ) {
+		// Filter product type by slug.
+		$tax_query = array();
+
+		// Filter product type by slug.
+		if ( ! empty( $request['brand'] ) ) {
+			$tax_query[] = array(
+				'taxonomy' => $this->taxonomy,
+				'field'    => 'slug',
+				'terms'    => $request['brand'],
+			);
+		}
+
+		if ( ! empty( $tax_query ) ) {
+			$args['tax_query'] = $tax_query;
+		}
+
+		return $args;
 	}
 }
