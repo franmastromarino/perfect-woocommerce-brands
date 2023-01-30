fbc865efe87ff1fc55832f12889226bc1ab3ea69
Juan <6652831+quadlayers@users.noreply.github.com>
2023-01-30 10:45:30 -0300

    Update project settings

--- bin/build.js
+++ bin/build.js
@@ -98,18 +98,72 @@ fs.rm('./' + pluginName, {recursive: true}, (err) => {
 		copyFolderRecursiveSync('./build', './' + pluginName);
 		//Put lib folder in pluginName folder
 		copyFolderRecursiveSync('./lib', './' + pluginName);
+		//Put vendor_packages folder in pluginName folder
+		copyFolderRecursiveSync('./vendor_packages', './' + pluginName);
 		//Put vendor folder in pluginName folder
 		copyFolderRecursiveSync('./vendor', './' + pluginName);
-		//Put composer folder in pluginName folder
-		copyFolderRecursiveSync('./vendor_packages', './' + pluginName);
 		//Put jetpack_vendor folder in pluginName folder
 		copyFolderRecursiveSync('./jetpack_vendor', './' + pluginName);
 		//Put pluginName.php script into a pluginName folder
 		copyFileSync(pluginName+'.php','./'+pluginName);
+		//Add templates folder if exists
+		fs.access("./templates", function(error) {
+			if (!error) {
+				//Put templates folder in pluginName folder
+				copyFolderRecursiveSync('./templates', './' + pluginName);
+			}
+		});
+		//Add languages folder if exists
+		fs.access("./languages", function(error) {
+			if (!error) {
+				//Put languages folder in pluginName folder
+				copyFolderRecursiveSync('./languages', './' + pluginName);
+			}
+		});
+		//Add assets folder if exists
+		fs.access("./assets", function(error) {
+			if (!error) {
+				//Put assets folder in pluginName folder
+				copyFolderRecursiveSync('./assets', './' + pluginName);
+			}
+		});
+		//Add uninstall.php file if exists
+		fs.access("./uninstall.php", function(error) {
+			if (!error) {
+				//Put pluginName.php script into a pluginName folder
+				copyFileSync('uninstall.php','./'+pluginName);
+			}
+		});
 
 		//Remove vendor/bin folder
 		fs.rm('./'+pluginName+'/vendor/bin',{recursive: true}, (err) => {
 			err ?? true;
 		});
+
+		/*
+		 * If have a wc parameter need to exclude wp-license-client and wp-notice-plugin-required on:
+		 * - File: vendor_packages/wp-license-client
+		 * - File: vendor_packages/wp-notice-plugin-required
+		 * - Row in pluginName.php
+		*/
+		const myArgs = process.argv.slice(2);
+		if('wc' == myArgs[0]){
+			//Remove vendor_packages/wp-license-client file
+			fs.rm('./'+pluginName+'/vendor_packages/wp-license-client.php',{recursive: true}, (err) => {
+				err ?? true;
+			});
+			//Remove vendor_packages/wp-notice-plugin-required file
+			fs.rm('./'+pluginName+'/vendor_packages/wp-notice-plugin-required.php',{recursive: true}, (err) => {
+				err ?? true;
+			});
+			//Read pluginName file
+			var filepath = './'+pluginName+'/'+pluginName+'.php';
+			var content = fs.readFileSync(filepath, 'utf-8');
+			// replace lines to white space
+			var newContent = content.replace("require_once __DIR__ . '/vendor_packages/wp-notice-plugin-required.php';", '');
+			var finalContent = newContent.replace("require_once __DIR__ . '/vendor_packages/wp-license-client.php';", '');
+			fs.writeFileSync(filepath, finalContent, 'utf-8');
+		}
+
 	});
 });
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,9 +1,15 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('Example test', async ({ page }) => {
-	await page.goto('http://localhost:8889/');
-
-	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle(/perfect-woocommerce-brands/);
+test('Check if menu exists', async ({ page }) => {
+	//Go to login page
+	await page.goto('http://localhost:8889/wp-admin');
+	//Insert credentials and login
+	await page.getByLabel('Username or Email Address').fill('admin');
+	await page.getByLabel('Password').fill('password');
+	await page.getByRole('button', { name: 'Log In' }).click();
+	//Search Plugin Init menu
+	await expect(
+		page.locator('.wp-menu-name:has-text("Plugin Init")')
+	).toBeVisible();
 });
