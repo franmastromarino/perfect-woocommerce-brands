61a7533ada59cbe69762da2f53e9c5393024cc8a
Francisco <francisco.mastromarino@urbiport.com>
2025-06-11 17:04:14 +0200

    fix: version update

--- perfect-woocommerce-brands.php
+++ perfect-woocommerce-brands.php
@@ -14,7 +14,7 @@
  * Tested up to:            6.8
  * Requires PHP:            5.6
  * WC requires at least:    4.0
- * WC tested up to:         9.8
+ * WC tested up to:         9.9
  */
 
 defined( 'ABSPATH' ) || exit;
