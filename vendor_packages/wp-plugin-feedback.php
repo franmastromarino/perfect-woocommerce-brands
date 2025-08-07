f019661abbd8d75eec8092f10a11fdfcedf0a4a9
Francisco <francisco.mastromarino@urbiport.com>
2025-08-07 10:02:31 +0200

    fix(QLD-419): update dependencies

--- vendor_packages/wp-plugin-feedback.php
+++ vendor_packages/wp-plugin-feedback.php
@@ -4,7 +4,7 @@ if ( class_exists( 'QuadLayers\\PluginFeedback\\Load' ) ) {
 	\QuadLayers\PluginFeedback\Load::instance()->add(
 		PWB_PLUGIN_FILE,
 		array(
-			'support_link' => 'https://wordpress.org/support/plugin/perfect-woocommerce-brands/',
+			'support_link' => 'https://quadlayers.com/account/support/?utm_source=pwb_plugin&utm_medium=plugin_feedback&utm_campaign=support&utm_content=feedback_form',
 		)
 	);
 }
